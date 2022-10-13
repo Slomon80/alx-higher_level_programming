92e9c93279fe1d1c2e8f83d3dd53d0ae5a367f2f
Slomon80 <johnslomon8@gmail.com>
2022-10-13 03:00:45 +0000

    a script that print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,5 +5,5 @@ const firstArgSize = Math.floor(process.argv[2]);
 if (!firstArgSize) console.log('Missing size');
 
 for (let i = 0; i < firstArgSize; i++) {
-  console.log('x'.repeat(firstArgSize));
+  console.log('X'.repeat(firstArgSize));
 }
